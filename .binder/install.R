66d3f683d98ca2f5e37f7a750636878aed1b739d
Ido Bar <ido.idobar@gmail.com>
2021-03-28 00:08:12 +1000

    Updated binder install.R file

---
+++ .binder/install.R
@@ -0,0 +1,6 @@
+install.packages(c(
+  "tidyverse", "pacman", "shiny", "rmarkdown",
+  "learnr",  "scales",  
+  "agricolae", "car", "MASS")
+)
+install.packages("R/SciDatAnalysis_0.1.0.tar.gz", repos = NULL, type="source")
